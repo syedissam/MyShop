0f5a4a59c9a6746951fee78418a1e78ceb8ab865
syedissam <45280974+syedissam@users.noreply.github.com>
2020-04-08 23:49:27 +0500

    Product Listings done

--- MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +37,7 @@ namespace MyShop.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -45,8 +46,16 @@ namespace MyShop.WebUI.Controllers
 
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
+
                 context.Insert(product);
                 context.Commit();
+
+
                 return RedirectToAction("Index");
             }
 
@@ -72,7 +81,7 @@ namespace MyShop.WebUI.Controllers
 
         }
         [HttpPost]
-        public ActionResult Edit (Product product,string Id)
+        public ActionResult Edit (Product product,string Id,HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
 
@@ -88,9 +97,14 @@ namespace MyShop.WebUI.Controllers
                     return View(product);
                 }
 
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
+
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
                 context.Commit();
